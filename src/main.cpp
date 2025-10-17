5f474bdc2c6f3a18b9dec64fb1cf873fea9e9f89
m-saliola <loliegaming9@gmail.com>
2025-10-17 17:16:57 -0400

    Add backface culling

--- src/main.cpp
+++ src/main.cpp
@@ -81,9 +81,13 @@ int main() {
     glfwSetWindowSizeCallback(window, resize_callback);
 
     gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
-    
+
     glEnable(GL_BLEND);
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+    glEnable(GL_CULL_FACE);
+    glCullFace(GL_BACK);
+    glFrontFace(GL_CCW);  
 
     glEnable(GL_DEPTH_TEST);
 
